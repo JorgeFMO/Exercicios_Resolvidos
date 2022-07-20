ad704ac3ec0a9035671d1595f8aa420d6b052a75
Jorge F M Oliveira <jorgefmo@hotmail.com>
2022-07-20 15:46:46 -0300

    C#

--- 5 - Média 1/Program.cs
+++ 5 - Média 1/Program.cs
@@ -1,31 +1,31 @@
 ﻿/**
-Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5
-e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B 
+tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
 
 Entrada
-O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
 
 Saída
-Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
-Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá
-"Presentation Error".
+Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
+Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 */
 
 using System;
 using System.Globalization;
 
-namespace uri1005 {
+namespace uri1006 {
     class Program {
         static void Main(string[] args) {
 
-            double A, B, media;
+            double A, B, C, media;
 
             A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            media = ((A * 3.5) + (B * 7.5)) / 11;
+            media = (A * 2.0 + B * 3.0 + C * 5) / 10.0;
 
-            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
