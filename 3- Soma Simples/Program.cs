cbc30ce9a64284d6538efbae59697d0afdf7838c
Jorge F M Oliveira <jorgefmo@hotmail.com>
2022-07-20 15:34:24 -0300

    C#

--- 3- Soma Simples/Program.cs
+++ 3- Soma Simples/Program.cs
@@ -1,31 +1,28 @@
 ﻿/**
-A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
-
-- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
 
 Entrada
-A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+O arquivo de entrada contém 2 valores inteiros.
 
 Saída
-Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. 
-Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
-caso contrário, você receberá "Presentation Error".
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. 
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 */
 
 using System;
-using System.Globalization;
 
-namespace uri1002 {
+namespace uri1003 {
     class Program {
         static void Main(string[] args) {
 
-            double R, A, pi = 3.14159;
+            int A, B, soma;
 
-            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
 
-            A = pi * R * R;
+            soma = A + B;
 
-            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("SOMA = " + soma);
         }
     }
 }
