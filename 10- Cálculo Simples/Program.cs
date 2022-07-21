e2f6e8a4749fdc796661eaff73c73404e4b81db7
Jorge F M Oliveira <jorgefmo@hotmail.com>
2022-07-21 17:20:52 -0300

    C#

---
+++ 10- Cálculo Simples/Program.cs
@@ -0,0 +1,38 @@
+﻿/**
+Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de 
+peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". 
+O valor deverá ser apresentado com 2 casas após o ponto.
+*/
+
+using System;
+using System.Globalization;
+
+namespace uri1010 {
+    class Program {
+        static void Main(string[] args) {
+
+            int cod1, cod2, qte1, qte2;
+            double preco1, preco2, total;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            cod1 = int.Parse(valores[0]);
+            qte1 = int.Parse(valores[1]);
+            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            cod2 = int.Parse(valores[0]);
+            qte2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            total = preco1 * qte1 + preco2 * qte2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
